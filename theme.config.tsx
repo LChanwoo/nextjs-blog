922e7da0ddf4b71a2abdeb812a0a99144de15755
CHANWOO LEE <chw210601@gmail.com>
2022-12-05 22:00:12 +0900

    no message

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 const YEAR = new Date().getFullYear();
+const meta = {
+  title: '이찬우의 Next.js Blog',
+  description: 'Next.js 포트폴리오 블로그',
+  image: '/images/22313.png'
+}
 const config: DocsThemeConfig = {
-  logo: <span>Aldrich's Portfolio Blog</span>,
+  logo: <strong>Aldrich's Portfolio Blog</strong>,
   project: {
     link: 'https://github.com/LChanwoo/nextjs-blog',
   },
@@ -13,6 +18,18 @@ const config: DocsThemeConfig = {
   footer: {
     text: `${YEAR}@Chanwoo Aldrich Lee, All rights reserved.`,
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="follow, index" />
+      <meta name="description" content={meta.description} />
+      <meta property="og:site_name" content={meta.title} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} key="title"/>
+      <meta property="og:image" content={meta.image} />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
 }
 
 export default config
