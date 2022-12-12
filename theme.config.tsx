51fce6cb127b20a7755a25e5a1af83af31c587be
CHANWOO LEE <chw210601@gmail.com>
2022-12-12 18:19:10 +0900

    fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -29,7 +29,7 @@ const config: DocsThemeConfig = {
     const { frontMatter } = useConfig()
     return(
     <>
-      {/* <title>Aldrich's Portfolio Blog</title> */}
+      <title>Aldrich's Portfolio Blog</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="robots" content="follow, index" />
       <meta name="description" content={meta.description} />
