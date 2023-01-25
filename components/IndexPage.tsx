e377a2e2fc2e0e4410b61d2f0a2925e8f5f047bb
CHANWOO LEE <chw210601@gmail.com>
2023-01-25 17:57:48 +0900

    인덱스 수정

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -17,6 +17,51 @@ export const IndexPage = () => {
 
             </div>
         </div>
+        <div>
+        <h3  className="flex items-center justify-center "> 기술 Stack </h3>
+
+<div  className="flex items-center justify-center ">
+  <img src="https://img.shields.io/badge/nodejs-green?style=flat&logo=Node.js&logoColor=white"/>
+  <img src="https://img.shields.io/badge/HTML5-red?style=flat&logo=HTML5&logoColor=white"/>
+  <img src="https://img.shields.io/badge/CSS3-blue?style=flat&logo=CSS3&logoColor=white"/>
+  <img src="https://img.shields.io/badge/JavaScript-yellow?style=flat&logo=JavaScript&logoColor=white"/>
+  <img src="https://img.shields.io/badge/TypeScript-blue?style=flat&logo=TypeScript&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Java-Purple?style=flat&logo=Java&logoColor=white"/>
+</div>
+<br/>
+<div  className="flex items-center justify-center ">
+  <img src="https://img.shields.io/badge/react-blue?style=flat&logo=react&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Next.js-yellow?style=flat&logo=Next.js&logoColor=white"/>
+  <img src="https://img.shields.io/badge/TailwindCss-blue?style=flat&logo=Tailwind CSS&logoColor=white"/>
+</div>
+<br/>
+<div className="flex items-center justify-center ">
+  <img src="https://img.shields.io/badge/NestJs-black?style=flat&logo=NestJs&logoColor=red"/>
+  <img src="https://img.shields.io/badge/Express.js-yellow?style=flat&logo=Express&logoColor=white"/>
+  <img src="https://img.shields.io/badge/SpringBoot-green?style=flat&logo=Spring Boot&logoColor=white"/>
+</div>
+<br/>
+<div className="flex items-center justify-center ">
+  <img src="https://img.shields.io/badge/PM2-black?style=flat&logo=PM2&logoColor=white"/>
+  <img src="https://img.shields.io/badge/npm-black?style=flat&logo=npm&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Axios-black?style=flat&logo=Axios&logoColor=white"/>
+  <img src="https://img.shields.io/badge/vercel-black?style=flat&logo=vercel&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Amazon AWS-black?style=flat&logo=Amazon AWS&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Amazon EC2-black?style=flat&logo=Amazon EC2&logoColor=white"/>
+</div>
+<br/>
+<h5 className="flex items-center justify-center "> 협업툴 </h5>
+<div  className="flex items-center justify-center ">
+  <img src="https://img.shields.io/badge/Notion-black?style=flat&logo=Notion&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Slack-black?style=flat&logo=Slack&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Figma-black?style=flat&logo=Figma&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Telegram-black?style=flat&logo=Telegram&logoColor=white"/>
+</div>
+
+<br/>
+<br/>
+<br/>
+        </div>
         </>
     )
 }
