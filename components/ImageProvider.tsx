12d77fab3ac1ae410824be4164c25e50ddfd5642
CHANWOO LEE <chw210601@gmail.com>
2023-01-26 00:01:46 +0900

    no message

--- components/ImageProvider.tsx
+++ components/ImageProvider.tsx
@@ -1,13 +1,19 @@
+import Head from "next/head"
 import Image from "next/image"
 
 export const ImageProvider = ({ src }) => {
     return (
-        <Image
-            src={src}
-            alt="profile"
-            width={400}
-            height={300}
-            className="justify-center items-center w-fit p-5"
-        />
+        <>
+            <Head>
+                <meta property="og:image" content={src} />
+            </Head>
+            <Image
+                src={src}
+                alt="profile"
+                width={400}
+                height={300}
+                className="justify-center items-center w-fit p-5"
+            />
+        </>
     )
 }
